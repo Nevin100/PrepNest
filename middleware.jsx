d15c956748aed694804fe51414a119d5bf59473a
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-09 11:07:53 +0530

    Setting docker and Convex db

--- middleware.jsx
+++ middleware.jsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { stackServerApp } from "./stack-server-app";
+import { stackServerApp } from "./stack";
 
 export async function middleware(request) {
   const user = await stackServerApp.getUser();
